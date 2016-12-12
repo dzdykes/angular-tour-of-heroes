d44b8812af865c54704f741a164742778e280b34
dzdykes <dzdykes88@gmail.com>
2016-12-12 09:10:57 -0800

    added routing and styles

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,32 +9,21 @@ import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            }
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
+        DashboardComponent,
         HeroDetailComponent,
-        HeroesComponent,
-        DashboardComponent
+        HeroesComponent
     ],
+    providers: [HeroService],
     bootstrap: [
         AppComponent
     ],
